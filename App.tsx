d7a06fddcc62cac95b293777a520f219fd0ca373
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-09-26 22:10:22 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigationRoutes } from './routes/StackNavigationRoutes';
+
+export default function App() {
+  return (
+  <NavigationContainer>
+   <StackNavigationRoutes/>
+  </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ screens/DetailsScreen.tsx
@@ -0,0 +1,17 @@
+import { NavigatorScreenParams, RouteProp } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Button, Text, View } from "react-native";
+import { StackParamList } from "../routes/StackNavigationRoutes";
+
+type Props = NativeStackScreenProps<StackParamList, "DetailsScreen">;
+
+export default function DetailsScreen({ navigation }: Props){
+    return (
+        <View>
+            <Text>Tela de Detalhes do Produto</Text>
+            <Text>Id do Produto: 1</Text>
+            <Text>Descrição: Mouse Gamer</Text>
+            <Button title="Voltar para a tela inicial" onPress={()=> navigation.navigate("HomeScreen")}/>
+        </View>
+    );
+}
